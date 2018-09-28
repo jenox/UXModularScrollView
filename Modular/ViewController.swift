f13597a4bbb01a234df2caa7d3a33a12b37ae0b4
Christian Schnorr <christian.schnorr@me.com>
2018-09-28 21:13:28 +0200

    Initial Commit

---
+++ Modular/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Modular
+//
+//  Created by Christian Schnorr on 28.09.18.
+//  Copyright © 2018 Christian Schnorr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
